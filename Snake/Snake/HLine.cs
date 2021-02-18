b94e0a4939727ff779c70d28ca04c89e1ed6b10e
Movix0 <evgenijryzkov406@gmail.com>
2021-02-18 17:26:58 +0300

    Created new class "Figure"
    Unite horizontal and vertocals lines

--- Snake/Snake/HLine.cs
+++ Snake/Snake/HLine.cs
@@ -6,14 +6,12 @@ using System.Threading.Tasks;
 
 namespace Snake
 {
-    class HLine
+    class HLine : Figure
     {
         int xLeft;
         int xRight;
         int y;
         char sym;
-
-        List<Point> pList = new List<Point>();
 
         public HLine(int xLeft_, int xRight_, int y_, char sym_)
         {
@@ -21,25 +19,12 @@ namespace Snake
             xRight = xRight_;
             y = y_;
             sym = sym_;
-        }
 
-        public void CalcLine()
-        {
-           for(int x=xLeft; x<=xRight; x++)
+            for (int x = xLeft; x <= xRight; x++)
             {
                 Point p1 = new Point(x, y, sym);
-                pList.Add( p1 );
-      /*          Console.WriteLine("\n{0}, {1}, {2} ", x, y, sym); */
+                pList.Add(p1);
             }
-        }
-        public void Draw()
-        {
-            CalcLine();
-            foreach(Point p in pList)
-            {
-                p.HDraw();
-            }
-
-        }
+    }
     }
 }
--- Snake/Snake/VLine.cs
+++ Snake/Snake/VLine.cs
@@ -6,37 +6,25 @@ using System.Threading.Tasks;
 
 namespace Snake
 {
-    class VLine
+    class VLine : Figure
     {
         int x;
         int yTop;
         int yDown;
         char sym;
 
-        List<Point> plist = new List<Point>();
         public VLine(int x_, int yTop_, int yDown_, char sym_)
         {
             x = x_;
             yTop = yTop_;
             yDown = yDown_;
             sym = sym_;
-        }
-        public void CalcLine()
-        {
-            for(int y = yTop; y<=yDown; y++)
-            {
-                Point p = new Point(x, y, sym);
-                plist.Add(p);
-            }
-        }
 
-        public void Draw()
-        {
-            CalcLine();
-            foreach(Point p in plist)
-            {
-                p.Draw();
-            }
+            for (int y = yTop; y <= yDown; y++)
+                {
+                    Point p = new Point(x, y, sym);
+                    pList.Add(p);
+                }
         }
     }
 }
